561d89d53fffd9537de43727ac0079ddfa8e79e6
+ <+>
2024-08-20 15:53:21 +0300

    Completed Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,45 @@
+import { MdMail } from "react-icons/md";
 import Navbar from "../navbar/Navbar.jsx";
-import {IoIosSearch} from "react-icons/io";
-// import MobileNavbar from "../navbar/MobileNavbar.jsx";
+import { IoIosSearch } from "react-icons/io";
+import { PiPhoneCall } from "react-icons/pi";
+import MobileNavbar from "../navbar/MobileNavbar.jsx";
 
 function Header() {
-    return (
-        <header>
+  return (
+    <header className="flex flex-col p-4">
+      <div className="flex justify-around p-4">
+        <img src="https://placehold.co/100x30/black/white" alt="Page Logo" />
 
-            <div>
+        <div className="hidden items-center gap-4 md:flex">
+          {/*Search Bar*/}
+          <div className="flex items-center gap-2 border-b-2 border-black">
+            <label htmlFor="search">
+              <IoIosSearch />
+            </label>
+            <input type="text" placeholder="Search here" id="search" className="outline-none"/>
+          </div>
 
-                <img src="https://placehold.co/100x30/black/white" alt="Page Logo"/>
+          {/*Contact Info*/}
+          <div className="px-4 flex items-center gap-4">
+            <a href="mailto:example@mail.com" target="_blank"
+            className="flex items-center gap-2"
+            >
+                
+            <MdMail/> <span> example@mail.com</span> 
+            </a>
+            <a href="tel:(000) 888-88"
+            className="flex items-center gap-2"
+            >
+             <PiPhoneCall />   <span>(000) 888-88</span></a>
+          </div>
+        </div>
+      </div>
 
-                <div>
+      <Navbar />
 
-                    {/*Search Bar*/}
-                    <div>
-                        <label htmlFor="search">
-                            <IoIosSearch/>
-                        </label>
-                        <input type="text" placeholder="Search here" id="search"/>
-                    </div>
-
-                    {/*Contact Info*/}
-                    <div>
-                        <a href="mailto:example@mail.com" target="_blank">example@mail.com</a>
-                        <a href="tel:(000) 888-88">(000) 888-88</a>
-                    </div>
-
-                </div>
-            </div>
-
-            <Navbar/>
-
-            {/*<MobileNavbar/>*/}
-        </header>
-    )
+      <MobileNavbar/>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/navbar/MobileNavbar.jsx
+++ src/components/navbar/MobileNavbar.jsx
@@ -1,26 +1,26 @@
-import {RxHamburgerMenu} from "react-icons/rx";
-import {navMenus} from "./navMenus.js";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { navMenus } from "./navMenus.js";
+import { useState } from "react";
 
 function MobileNavbar() {
-    return (
-        <div>
-            <button>
-                <RxHamburgerMenu/>
-            </button>
-
-            <div>
-                {
-                    navMenus.map((menu, index) => (
-                        <a href={menu.link} key={index}>
-                            <menu.icon/>
-                            {menu.name}
-                        </a>
-                    ))
-                }
-            </div>
-
-        </div>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="md:hidden self-end">
+      <button onClick={() => setIsOpen(!isOpen)}>
+        <RxHamburgerMenu />
+      </button>
+      {isOpen && (
+        <nav className="absolute right-4 flex w-fit flex-col gap-2 bg-gray-100 p-4 divide-y divide-gray-500 rounded-xl ">
+          {navMenus.map((menu, index) => (
+            <a href={menu.link} key={index} className="flex items-center gap-2 hover:bg-gray-200 cursor-pointer">
+              <menu.icon />
+              {menu.name}
+            </a>
+          ))}
+        </nav>
+      )}
+    </div>
+  );
 }
 
-export default MobileNavbar
+export default MobileNavbar;
